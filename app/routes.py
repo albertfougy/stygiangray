0c5f95628196f606d5e2daf34e447e3d0a810546
Albert Fougy <albert@fougy.com>
2019-04-14 17:16:33 -0400

    added POST method to decorator

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import flash, render_template, redirect, url_for
 from app.forms import LoginForm
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,14 +17,15 @@ def index():
             'body': 'I am Iron Man'
         }
     ]
-    
+
     return render_template('index.html', title='The Home of', user=user, posts=posts)
 
-@app.route('/login', methods=['GET','POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
-              form.username.data, form.remember_me.data))
+            form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
